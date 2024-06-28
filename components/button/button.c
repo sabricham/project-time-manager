3148efa46e4de4f99f5241ff8309e333d26252cd
Sabri Chamtouri <149872304+sabricham@users.noreply.github.com>
2024-06-28 10:49:47 +0200

    button implemented

--- components/button/button.c
+++ components/button/button.c
@@ -7,36 +7,35 @@
 #include "esp_log.h"
 
 /* Components */
-#include "sample.h"
+#include "button.h"
 
 #include "queue_handler.h"
 #include "task_handler.h"
 
 /* Private variables & defines */
-#define TAG "Sample"
-QueueHandle_t sample_queue = NULL;
-queue_message sample_queue_message;
+#define TAG "Button"
+QueueHandle_t button_queue = NULL;
+queue_message button_queue_message;
 
 /* Public variables & defines */
-
 
 /* Private functions & routines */
 
 /* Public functions & routines */
 
-void sample_task()
+void button_task()
 {
     ESP_LOGI(TAG, "Starting task");
 
     esp_task_wdt_add(NULL);
-    start_queue(&sample_queue, &sample_queue_message, 10, TAG);
+    start_queue(&button_queue, &button_queue_message, 10, TAG);
 
     ESP_LOGI(TAG, "Task started correctly");
 
     while(1)
     {
         // Get messages from other tasks
-        if(xQueueReceive(sample_queue, &sample_queue_message, 0))
+        if(xQueueReceive(button_queue, &button_queue_message, 0))
         {
             ESP_LOGI(TAG, "Received message");
         }
---
+++ components/button/button.h
@@ -0,0 +1,12 @@
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+/* Components */
+
+/* Public variables & defines */
+
+/* Public functions & routines */
+
+void button_task();
+
+#endif /* _BUTTON_H_ */
